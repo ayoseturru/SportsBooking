49163f1018c47d2e61a38e82f8767fb47699763f
ayoseturru <ayose_gladiator@hotmail.com>
2016-03-12 11:29:08 +0000

    Now free columns is set to false by default

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,4 +1,3 @@
 class Installation < ActiveRecord::Base
   has_and_belongs_to_many :sports, dependent: :destroy
-  belongs_to :sports_installation
 end
--- app/models/sports_installations_time_band.rb
+++ app/models/sports_installations_time_band.rb
@@ -1,4 +1,10 @@
 class SportsInstallationsTimeBand < ActiveRecord::Base
   belongs_to :sports_installation
   belongs_to :time_band
+
+  before_save :set_free
+  private
+  def set_free
+    self.free ||= true
+  end
 end
